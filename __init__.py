55ecec3f791cdd5aede0c4a2e6e0c03ee985d339
Rina <contact.rina@icloud.com>
2026-08-12 01:01:07 +0200

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,13 @@
+"""
+HERMES
+
+Holistic Evidence-based Research for Mobility Evaluation and Simulation
+"""
+
+import hermes.logger
+
+__version__ = "0.2.0"
+
+__author__ = "Rina Razafimahefa"
+
+__license__ = "MIT"
